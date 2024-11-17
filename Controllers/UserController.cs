a1d04f8b5ba1f86391505b9b4e2d7a2943822ea2
Ayoub Nasri <nas.ayou123@gmail.com>
2024-11-17 17:31:08 +0100

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Mvc;
+using TrackerDeFavorisApi.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace TrackerDeFavorisApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _userManager;
+        public UserController(UserContext userManager){
+            _userManager = userManager;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id){
+            var user = await _userManager.Users.FindAsync(id);
+            if (user == null){
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+    [HttpGet("User")]
+    public async Task<ActionResult<List<string>>> GetUsers()
+    {
+        var usersprenom = await _userManager.Users.Select(u => u.Id).ToListAsync();
+
+        if (usersprenom == null || usersprenom.Count == 0)
+        {
+            return NotFound("No users found.");
+        }
+
+        return Ok(usersprenom);
+    }
+    [HttpGet("Admin")]
+    public async Task<ActionResult<string>> GetAdmins(){
+        var admins = await _userManager.Users.Where(u => u.RoleType == 0).Select(u => u.Id).ToListAsync();
+        if (admins == null || admins.Count == 0){
+            return NotFound("No users Found.");
+        }
+        else{
+            return Ok(admins);
+        }
+
+    }
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> PostUser(Userinfo userinfo) {
+        var user = new User();
+        user.Id = userinfo.Prenom;
+        user.MotDePasse = userinfo.MotDePasse;
+        _userManager.Users.Add(user);
+        await _userManager.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUser), new { Id = user.Id }, user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult> PostLogin(Userinfo userinfo){
+        var found = await _userManager.Users.FirstOrDefaultAsync(u => userinfo.Prenom == u.Id && userinfo.MotDePasse == u.MotDePasse);
+        if (found != null){
+            return Ok($"Welcom {userinfo.Prenom}");
+        }
+        else{
+            return NotFound("User not in the DataBase");
+        }
+    }
+
+    [HttpPut(" ")]
+    public async Task<ActionResult<User>> PutUser(Userinfo userinfo){
+        var user = await _userManager.Users.FindAsync(userinfo.Prenom);
+        if (user == null){
+            return NotFound();
+        }
+        user.Prenom = userinfo.Prenom;
+        user.MotDePasse = userinfo.MotDePasse;
+        _userManager.Entry(user).State = EntityState.Modified;
+        try
+            {
+                await _userManager.SaveChangesAsync();
+            }
+        catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(500, "Erreur de concurrence");
+            }
+        return Ok(user);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(string id){
+        var user = await _userManager.Users.FindAsync(id);
+        if (user == null){
+            return NotFound();
+        }
+        _userManager.Users.Remove(user);
+        await _userManager.SaveChangesAsync();
+        return NoContent();
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.EntityFrameworkCore.Sqlite;
+using TrackerDeFavorisApi.Models;
+namespace TrackerDeFavorisApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        // Adding the DataBase
+        builder.Services.AddDbContext<UserContext>();
+        builder.Services.AddDbContext<FilmContext>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
